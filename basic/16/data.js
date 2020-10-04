5aa0949e26a4787fc1669afe0cbf3261b0f2f7b9
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2020-10-04 18:30:07 +0530

    bs

---
+++ basic/16/data.js
@@ -0,0 +1,15 @@
+async function csvData() {
+    const response = await fetch('trial.csv');
+    const data = await response.text();
+
+    const rawData = data.split('\n').slice(1);
+    rawData.forEach(rows => {
+        const columns = rows.split(',');
+        const yr = columns[0];
+        // array.push(yr);
+        const temp = columns[1];
+        console.log(yr, temp);
+    })
+}
+
+csvData();
