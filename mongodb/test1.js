eaa195932bf822d223cd746270daa84839a951bf
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2020-10-22 17:51:08 +0530

    rest api

---
+++ mongodb/test1.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { urlencoded, json } = require('body-parser');
+
+
+
+const dogs = new mongoose.Schema({
+    breed: {
+        type: String,
+        fur: Boolean,
+    },
+
+    food: {
+        type: String,
+        weight: Number
+    }
+
+})
+
+const Dog = new mongoose.model('doge', dogs)
+
+app.use(morgan('dev'));
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+function connect() {
+    return mongoose.connect('mongodb://localhost:27017/simple_stuff');
+}
+
+app.get('/doge', async(res, req) => {
+    console.log('checking...');
+    const inu = await Dog.find({}).lean().exec();
+    res.status(200).json(inu);
+})
+
+connect()
+    .then(async connection => {
+        app.listen(5000, () => {
+            console.log("checking...")
+        })
+    })
+    .catch(err => console.err(err));
