4ddfb4b783a9a0266aea6df84b72717614871830
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2020-10-22 10:52:20 +0530

    querying with filters

--- mongodb/test.js
+++ mongodb/test.js
@@ -21,7 +21,10 @@ const student = new mongoose.Schema({
 
 
 const school = new mongoose.Schema({
-    name: String
+    name: String,
+    principal: String,
+    students: Number,
+    isGreat: Boolean,
 })
 
 
@@ -31,15 +34,52 @@ const Student = new mongoose.model('student', student);
 
 connect()
     .then(async connection => {
+        const school1 = {
+            name: 'kdav',
+            principal: 'manas',
+            students: 2000,
+            isGreat: true
+        }
 
-        const school = await School.create({ name: 'DPS' });
-        const deets = await Student.create({ firstName: 'Anthony', school: school._id });
-        // const ID = await Student.findById('5f9022faf78b423515df8fa6'); //felix's id
+        const school2 = {
+            name: 'tiad',
+            principal: 'john',
+            students: 700,
+            isGreat: false
+        }
 
-        const pop = await Student.findById(deets.id)
-            .populate('school')
+        const school3 = {
+            name: "qsad",
+            principal: "johnson",
+            students: 400,
+            isGreat: true,
+        };
+
+        /*
+
+        querying associations with populate
+
+         // const school = await School.create({ name: 'DPS' });
+         // const deets = await Student.create({ firstName: 'Anthony', school: school._id });
+         // // const ID = await Student.findById('5f9022faf78b423515df8fa6'); //felix's id
+
+         // const pop = await Student.findById(deets.id)
+         //     .populate('school')
+         //     .exec()
+
+         // console.log(pop);
+
+         
+         */
+
+        const schools = await School.create([school1, school2, school3]);
+        // const find = await School.findOne({ students: { $lt: 1000 }, isGreat: true }).exec(); // qsad
+
+        const find = await School.find({ students: { $lt: 1000 } })
+            .sort({ students: 1 }) // ascending
             .exec()
 
-        console.log(pop);
+        console.log(find);
+        // console.log(schools);
     })
     .catch(err => console.error(err));
